c103871091d7754c086e182d323fed99ab77c4e1
Harsh Ranjan <shriharshranjangupta@gmail.com>
2024-06-22 21:09:04 +0530

    intial file

---
+++ index.js
@@ -0,0 +1,22 @@
+const express =require('express');
+
+const url = `https://yourappname123.onrender.com/`;
+
+
+
+const interval = 30000;
+
+function reloadWebsite() {
+  axios.get(url)
+    .then(response => {
+      console.log(`Reloaded at ${new Date().toISOString()}: Status Code ${response.status}`);
+    })
+    .catch(error => {
+      console.error(`Error reloading at ${new Date().toISOString()}:`, error.message);
+    });
+}
+
+setInterval(reloadWebsite, interval);
+
+//
+
